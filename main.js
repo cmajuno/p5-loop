a288a06be4095a481d6dbdf791b7bde0857783c9
cmajuno <84113087+cmajuno@users.noreply.github.com>
2021-05-13 04:46:11 +0200

    use for loop
    use for loop to draw many circles . effective than while loop

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 function setup(){
   createCanvas(500,500);
 }
-let x = 20
+//let x = 20
 function draw (){
   background (100,0,200);
   noStroke();
   fill (200,100,10);
-  while (x < width){
+  for (x=20; x <=width; x+=40){
     circle (x,100,30);
-    x+=40
   }
+  //while (x < width){
+  //  circle (x,100,30);
+  //  x+=40
+  //}
   
 }
